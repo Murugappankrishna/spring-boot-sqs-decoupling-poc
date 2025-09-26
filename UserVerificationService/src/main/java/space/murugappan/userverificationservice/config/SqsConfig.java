89e16356fd7cdc6275034e073d9169bb8517043f
MurugappanKrishna <murugappankrishna@icloud.com>
2025-09-26 19:40:56 +0530

    Refractor:Remove Batch Processing

--- UserVerificationService/src/main/java/space/murugappan/userverificationservice/config/SqsConfig.java
+++ UserVerificationService/src/main/java/space/murugappan/userverificationservice/config/SqsConfig.java
@@ -1,13 +1,12 @@
 package space.murugappan.userverificationservice.config;
 
 import io.awspring.cloud.sqs.config.SqsMessageListenerContainerFactory;
-import io.awspring.cloud.sqs.listener.ListenerMode;
 import io.awspring.cloud.sqs.listener.acknowledgement.handler.AcknowledgementMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
-import java.time.Duration;
+
 
 
 @Configuration
@@ -25,10 +24,6 @@ public class SqsConfig {
                 .sqsAsyncClient(sqsAsyncClient)
                 .configure(options->
                         options.acknowledgementMode(AcknowledgementMode.MANUAL)
-                                .maxMessagesPerPoll(10)
-                                .maxDelayBetweenPolls(Duration.ofSeconds(10))
-                                .pollTimeout(Duration.ofSeconds(10))
-                                .listenerMode(ListenerMode.BATCH)
                 )
                 .build();
     }
--- UserVerificationService/src/main/java/space/murugappan/userverificationservice/service/UserRegistrationSqsListener.java
+++ UserVerificationService/src/main/java/space/murugappan/userverificationservice/service/UserRegistrationSqsListener.java
@@ -1,30 +1,19 @@
 package space.murugappan.userverificationservice.service;
 
 import io.awspring.cloud.sqs.annotation.SqsListener;
-import io.awspring.cloud.sqs.listener.acknowledgement.BatchAcknowledgement;
-import org.springframework.stereotype.Service;
+import io.awspring.cloud.sqs.listener.acknowledgement.Acknowledgement;
 
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
+import org.springframework.stereotype.Service;
+import space.murugappan.userverificationservice.dao.User;
+
+
 
 @Service
 public class UserRegistrationSqsListener {
-    Executor ioExecutor =  Executors.newFixedThreadPool(10);
     @SqsListener(value = "learn_sqs.fifo", factory = "defaultSqsListenerContainerFactory")
-    public void listen(List<String> messages, BatchAcknowledgement <String> batchAcknowledgement) {
-        System.out.println(messages.size());
-        messages.forEach(message -> CompletableFuture.runAsync(()->{
-
-            try {
-                Thread.sleep((long) (Math.random() * 100));
-                System.out.println(message);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        },ioExecutor));
-        batchAcknowledgement.acknowledgeAsync();
+    public void listen(User registeredUser, Acknowledgement acknowledgement) {
+        System.out.println(registeredUser);
+        acknowledgement.acknowledgeAsync();
     }
 
 }
